5c517e4ffd6638023a82af76069ac90db4f87aa3
Marek <marek.skrzypkowski98@gmail.com>
2020-03-30 09:44:50 +0200

    initial commit

---
+++ src/test/java/NoteTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NoteTest
+{
+    
+    @Test
+    void of()
+    {
+    }
+    
+    @Test
+    void getName()
+    {
+    }
+    
+    @Test
+    void getNote()
+    {
+    }
+}
