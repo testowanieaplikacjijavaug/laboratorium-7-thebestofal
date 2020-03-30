8737804fe7f664ebc1aa0490aa06dc12634088a1
Marek <marek.skrzypkowski98@gmail.com>
2020-03-30 10:12:07 +0200

    NoteTest added

--- src/test/java/NoteTest.java
+++ src/test/java/NoteTest.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -5,18 +7,60 @@ import static org.junit.jupiter.api.Assertions.*;
 class NoteTest
 {
     
+    static Note note;
+    private Throwable thrown;
     @Test
     void of()
     {
+        note = Note.of("Marek", 5);
+        note = Note.of("Marek", 4);
+        assertEquals(note.getNote(), 4);
+    }
+    
+    @Test
+    void of_incorrectNote()
+    {
+        thrown = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            note = Note.of("Marek", 1);
+        });
+        assertEquals("Niewłaściwa ocena", thrown.getMessage());
+    }
+    
+    @Test
+    void of_NullStudentName()
+    {
+        thrown = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            note = Note.of(null, 4);
+        });
+        assertEquals("Imię ucznia nie może być null", thrown.getMessage());
+    }
+    
+    @Test
+    void of_EmptyStudentName()
+    {
+        thrown = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            note = Note.of("", 4);
+        });
+        assertEquals("Imię ucznia nie może być puste", thrown.getMessage());
     }
     
     @Test
     void getName()
     {
+        note = Note.of("Marek", 5);
+        assertEquals(note.getName(), "Marek");
     }
     
     @Test
     void getNote()
     {
+        note = Note.of("Marek", 6);
+        assertEquals(note.getNote(), 6);
+    }
+    
+    @AfterAll
+    static public void closeUp()
+    {
+        note = null;
     }
 }
